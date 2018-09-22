fdbb0481a1f8765f38f5ac332b789ebe337484e3
ahmaadidrees <38511305+ahmaadidrees@users.noreply.github.com>
2018-09-21 22:43:28 -0700

    Add files via upload

---
+++ DS4/SelectionSort.java
@@ -0,0 +1,22 @@
+public class SelectionSort implements SortingAlgorithm{
+	
+	public void sort(int [] a)
+    {
+        int n = a.length;
+ 
+        for (int i = 0; i < n-1; i++)
+        {
+            int min = i;
+            for (int j = i+1; j < n; j++){
+                if (a[j] < a[min]){
+                    min = j;
+                }
+            }
+ 
+   
+            int temp = a[min];
+            a[min] = a[i];
+            a[i] = temp;
+        }
+    }
+}
